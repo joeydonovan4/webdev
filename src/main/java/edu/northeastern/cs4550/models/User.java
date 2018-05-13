66e76ac930611da92ecf76722ef0ff1e51ed3b9a
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-13 14:51:06 -0400

    Add User model & use spaces in pom.xml

---
+++ src/main/java/edu/northeastern/cs4550/models/User.java
@@ -0,0 +1,35 @@
+package edu.northeastern.cs4550.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    private String email;
+
+    private String role;
+
+    private Date dateOfBirth;
+}
