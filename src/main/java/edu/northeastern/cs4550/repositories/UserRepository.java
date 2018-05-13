c9d342675ede9dbc1e7f0d4688e666b8cb19caea
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-13 14:54:00 -0400

    Add UserRepository

---
+++ src/main/java/edu/northeastern/cs4550/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package edu.northeastern.cs4550.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.northeastern.cs4550.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {}
