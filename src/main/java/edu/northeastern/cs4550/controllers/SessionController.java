701e87c4f4553a49443ec6adb6e0399c262dfb57
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-13 17:59:24 -0400

    Endpoint for logging in a user

---
+++ src/main/java/edu/northeastern/cs4550/controllers/SessionController.java
@@ -0,0 +1,35 @@
+package edu.northeastern.cs4550.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.security.auth.login.FailedLoginException;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import edu.northeastern.cs4550.models.User;
+import edu.northeastern.cs4550.services.IUserService;
+
+@RestController
+@RequestMapping("/api/session")
+public class SessionController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@Valid @RequestBody User user, HttpSession session)
+            throws FailedLoginException{
+        User existingUser =
+                userService.findUserByUsernameAndPassword(user.getUsername(), user.getPassword());
+        if (existingUser == null) {
+            throw new FailedLoginException("Incorrect password.");
+        }
+        session.setAttribute("user", user);
+        return ResponseEntity.ok(user);
+    }
+}
