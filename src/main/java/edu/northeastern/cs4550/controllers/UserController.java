2e2023ab92ee86d7275a5509d2320e6866babd00
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-13 16:01:57 -0400

    Basic CRUD operations endpoints for User

---
+++ src/main/java/edu/northeastern/cs4550/controllers/UserController.java
@@ -0,0 +1,61 @@
+package edu.northeastern.cs4550.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import edu.northeastern.cs4550.models.User;
+import edu.northeastern.cs4550.services.IUserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAllUsers() {
+        List<User> allUsers = userService.findAllUsers();
+        return ResponseEntity.ok(allUsers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserByID(@PathVariable(value = "id") int id) {
+        User foundUser = userService.findUserByID(id);
+        return ResponseEntity.ok(foundUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateUser(@PathVariable(value = "id") int id,
+                           @Valid @RequestBody User user) {
+        if (id != user.getId()) {
+            return ResponseEntity.badRequest().body("ID in path does not match ID in user object");
+        }
+        User updatedUser = userService.updateUser(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable(value = "id") int id) {
+        User deletedUser = userService.deleteUser(id);
+        return ResponseEntity.ok(deletedUser);
+    }
+}
