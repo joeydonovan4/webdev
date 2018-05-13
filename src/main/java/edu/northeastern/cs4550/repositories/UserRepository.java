17c5ba30f351273177a00e942bc90944aba8f5ec
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-13 17:40:02 -0400

    Repository method to find User by un & pw

--- src/main/java/edu/northeastern/cs4550/repositories/UserRepository.java
+++ src/main/java/edu/northeastern/cs4550/repositories/UserRepository.java
@@ -9,4 +9,6 @@ import edu.northeastern.cs4550.models.User;
 public interface UserRepository extends JpaRepository<User,Integer> {
 
     User findByUsername(String username);
+
+    User findByUsernameAndPassword(String username, String password);
 }
