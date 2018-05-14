9e145e92c0bf4545a0004eae76373b72c928746d
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-14 12:01:32 -0400

    Create user controller function

--- src/main/webapp/jquery/components/admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/admin/user-admin.controller.client.js
@@ -7,7 +7,22 @@
     $(main);
 
     function main() { … }
-    function createUser() { … }
+
+    function createUser() {
+        var username = $('#usernameFld').val();
+        var password = $('#passwordFld').val();
+        var firstName = $('#firstNameFld').val();
+        var lastName = $('#lastNameFld').val();
+        var email = $('#emailFld').val();
+        var phone = $('#phoneFld').val();
+        var dateOfBirth = $('#dobFld').val();
+        var role = $('#roleFld').val();
+
+        userService.createUser(
+            new User(username, password, firstName, lastName, email, phone, role, dateOfBirth)
+        );
+    }
+
     function findAllUsers() { … }
     function findUserById() { … }
     function deleteUser() { … }
