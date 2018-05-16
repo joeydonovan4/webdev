8b25d8b486860dce7e0b4330e4b421408f616281
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-16 16:42:04 -0400

    Session service

---
+++ src/main/webapp/jquery/services/session.service.client.js
@@ -0,0 +1,19 @@
+function SessionServiceClient() {
+    this.getLoggedInUser = getLoggedInUser;
+    this.login = login;
+    this.logout = logout;
+    this.sessionURI = '/api/session';
+    var self = this;
+
+    function getLoggedInUser() {
+
+    }
+
+    function login() {
+
+    }
+
+    function logout() {
+
+    }
+}
