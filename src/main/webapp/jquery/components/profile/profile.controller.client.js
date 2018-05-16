b876d7b18a75df7602f25105ef1e3aef95cf39b7
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-16 17:15:46 -0400

    Logout and showing user data on profile page

--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -1,15 +1,51 @@
 (function () {
+    var $firstName, $lastName;
+    var $email, $username;
+    var $password, $phone;
+    var $dateOfBirth, $role;
+    var profileService = new ProfileServiceClient();
+    var sessionService = new SessionServiceClient();
+    var loginPage = '/jquery/components/login/login.template.client.html';
     $(main);
 
     function main() {
+        $firstName = $('#first-name-fld');
+        $lastName = $('#last-name-fld');
+        $email = $('#email-fld');
+        $username = $('#username-fld');
+        $password = $('#password-fld');
+        $phone = $('#phone-fld');
+        $dateOfBirth = $('#dob-fld');
+        $role = $('#role-fld');
 
+        $('#edit-btn').click(updateProfile);
+        $('#logout-btn').click(logout);
+        getLoggedInUser();
+    }
+
+    function getLoggedInUser() {
+        profileService.getLoggedInUser().then(showUserData);
+    }
+
+    function showUserData(user) {
+        $firstName.val(user.firstName);
+        $lastName.val(user.lastName);
+        $email.val(user.email);
+        $username.val(user.username);
+        $password.val(user.password);
+        $phone.val(user.phone);
+        $dateOfBirth.val(user.dateOfBirth);
     }
 
     function logout() {
+        sessionService.logout().then(redirectToLogin);
+    }
 
+    function redirectToLogin() {
+        document.location.href = loginPage;
     }
 
     function updateProfile() {
-
+        profileService.updateProfile();
     }
 })();
