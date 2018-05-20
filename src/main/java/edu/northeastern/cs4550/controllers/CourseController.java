deb9271eb23ac51190228d5bca8943111c0da9ff
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-20 16:13:47 -0400

    Course controller endpoints
    - Creating a module for a course
    - Finding all modules for a course

--- src/main/java/edu/northeastern/cs4550/controllers/CourseController.java
+++ src/main/java/edu/northeastern/cs4550/controllers/CourseController.java
@@ -16,7 +16,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import edu.northeastern.cs4550.models.Course;
+import edu.northeastern.cs4550.models.Module;
 import edu.northeastern.cs4550.services.ICourseService;
+import edu.northeastern.cs4550.services.IModuleService;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -24,6 +26,9 @@ public class CourseController {
 
     @Autowired
     private ICourseService courseService;
+
+    @Autowired
+    private IModuleService moduleService;
 
     @PostMapping
     public ResponseEntity<Course> createCourse(@Valid @RequestBody Course course) {
@@ -57,5 +62,19 @@ public class CourseController {
         }
         Course updatedCourse = courseService.updateCourse(course);
         return ResponseEntity.ok(updatedCourse);
+    }
+
+    @PostMapping("/{id}/modules")
+    public ResponseEntity<Module> createModule(@PathVariable(value = "id") int courseId,
+                                               @Valid @RequestBody Module module) {
+        Module createdModule = moduleService.createModule(courseId, module);
+        return ResponseEntity.ok(createdModule);
+    }
+
+    @GetMapping("/{id}/modules")
+    public ResponseEntity<List<Module>> findAllModulesForCourse(
+            @PathVariable(value = "id") int courseId) {
+        List<Module> courseModules = moduleService.findAllModulesForCourse(courseId);
+        return ResponseEntity.ok(courseModules);
     }
 }
