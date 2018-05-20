4a56e06be654d1912ace481f120ae1683387da9e
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-20 16:29:26 -0400

    Add Lesson model & update Module model

--- src/main/java/edu/northeastern/cs4550/models/Lesson.java
+++ src/main/java/edu/northeastern/cs4550/models/Lesson.java
@@ -2,20 +2,17 @@ package edu.northeastern.cs4550.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class Module extends Audit {
+public class Lesson extends Audit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,8 +22,5 @@ public class Module extends Audit {
 
     @ManyToOne
     @JsonIgnore
-    private Course course;
-
-    @OneToMany(mappedBy = "module")
-    private List<Lesson> lessons;
+    private Module module;
 }
