f339897ba54ae92c34f694d7ff6f1227b69097d9
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-28 15:53:23 -0400

    Widget endpoints

--- src/main/java/edu/northeastern/cs4550/controllers/LessonController.java
+++ src/main/java/edu/northeastern/cs4550/controllers/LessonController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import edu.northeastern.cs4550.models.Lesson;
+import edu.northeastern.cs4550.models.Widget;
 import edu.northeastern.cs4550.services.ILessonService;
+import edu.northeastern.cs4550.services.IWidgetService;
 
 @RestController
 @RequestMapping("/api/lessons")
@@ -23,6 +26,9 @@ public class LessonController {
 
     @Autowired
     private ILessonService lessonService;
+
+    @Autowired
+    private IWidgetService widgetService;
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Lesson> deleteLesson(@PathVariable(value = "id") int id) {
@@ -50,5 +56,20 @@ public class LessonController {
         }
         Lesson updatedLesson = lessonService.updateLesson(lesson);
         return ResponseEntity.ok(updatedLesson);
+    }
+
+    @PostMapping("/{lid}/topics/{tid}/widgets")
+    public ResponseEntity<Widget> createWidget(@PathVariable(value = "lid") int lessonId,
+                                               @PathVariable(value = "tid") int topicId,
+                                               @Valid @RequestBody Widget widget) {
+        Widget createdWidget = widgetService.createWidget(lessonId, topicId, widget);
+        return ResponseEntity.ok(createdWidget);
+    }
+
+    @GetMapping("/{lid}/topics/{tid}/widgets")
+    public ResponseEntity<List<Widget>> findAllWidgetsForLessonTopic(
+            @PathVariable(value = "lid") int lessonId, @PathVariable(value = "tid") int topicId) {
+        List<Widget> topicWidgets = widgetService.findAllWidgetsForLessonTopic(lessonId, topicId);
+        return ResponseEntity.ok(topicWidgets);
     }
 }
---
+++ src/main/java/edu/northeastern/cs4550/controllers/WidgetController.java
@@ -0,0 +1,54 @@
+package edu.northeastern.cs4550.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import edu.northeastern.cs4550.models.Widget;
+import edu.northeastern.cs4550.services.IWidgetService;
+
+@RestController
+@RequestMapping("/api/widgets")
+public class WidgetController {
+
+    @Autowired
+    private IWidgetService widgetService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Widget> deleteWidget(@PathVariable(value = "id") int id) {
+        Widget deletedWidget = widgetService.deleteWidget(id);
+        return ResponseEntity.ok(deletedWidget);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Widget>> findAllWidgets() {
+        List<Widget> allWidgets = widgetService.findAllWidgets();
+        return ResponseEntity.ok(allWidgets);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Widget> findWidgetById(@PathVariable(value = "id") int id) {
+        Widget widget = widgetService.findWidgetById(id);
+        return ResponseEntity.ok(widget);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateWidget(@PathVariable(value = "id") int id,
+                                       @Valid @RequestBody Widget widget) {
+        if (id != widget.getId()) {
+            return ResponseEntity.badRequest().body("ID in path does not match ID in Widget object");
+        }
+        Widget updatedWidget = widgetService.updateWidget(widget);
+        return ResponseEntity.ok(updatedWidget);
+    }
+}
