99a5f260699616208a329df7366522dc49867253
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-28 15:18:43 -0400

    Topic service impl

--- src/main/java/edu/northeastern/cs4550/services/TopicService.java
+++ src/main/java/edu/northeastern/cs4550/services/TopicService.java
@@ -1,41 +1,64 @@
 package edu.northeastern.cs4550.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+import edu.northeastern.cs4550.models.Lesson;
 import edu.northeastern.cs4550.models.Topic;
+import edu.northeastern.cs4550.repositories.TopicRepository;
+import edu.northeastern.cs4550.utils.ResourceNotFoundException;
 
 @Service
 public class TopicService implements ITopicService {
 
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @Autowired
+    private ILessonService lessonService;
+
     @Override
     public Topic createTopic(int lessonId, Topic topic) {
-        return null;
+        Lesson lesson = lessonService.findLessonById(lessonId);
+        topic.setLesson(lesson);
+        return topicRepository.save(topic);
     }
 
     @Override
     public Topic deleteTopic(int id) {
-        return null;
+        Topic existingTopic = findTopicById(id);
+        topicRepository.delete(existingTopic);
+        return existingTopic;
     }
 
     @Override
     public List<Topic> findAllTopics() {
-        return null;
+        return topicRepository.findAll();
     }
 
     @Override
     public Topic findTopicById(int id) {
-        return null;
+        Optional<Topic> topic = topicRepository.findById(id);
+        if (!topic.isPresent()) {
+            throw new ResourceNotFoundException(Topic.class, "id", Integer.toString(id));
+        }
+        return topic.get();
     }
 
     @Override
     public List<Topic> findAllTopicsForLesson(int lessonId) {
-        return null;
+        lessonService.findLessonById(lessonId);
+        return topicRepository.findByLessonId(lessonId);
     }
 
     @Override
     public Topic updateTopic(Topic topic) {
-        return null;
+        Topic existingTopic = findTopicById(topic.getId());
+        existingTopic.setTitle(topic.getTitle());
+        existingTopic.setLesson(topic.getLesson());
+        return topicRepository.save(existingTopic);
     }
 }
