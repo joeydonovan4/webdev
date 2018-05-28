a3987d1d44435a57f739e82657cd676398ef3ee6
Joseph Donovan <joeydonovan4@gmail.com>
2018-05-28 12:02:08 -0400

    Add Topic model

---
+++ src/main/java/edu/northeastern/cs4550/models/Topic.java
@@ -0,0 +1,26 @@
+package edu.northeastern.cs4550.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Topic extends Audit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    @ManyToOne
+    @JsonIgnore
+    private Lesson lesson;
+}
