b7255ea5d5acc3f90f3f1da1229e1f863e732963
Joseph Donovan <joeydonovan4@gmail.com>
2018-06-05 14:34:45 -0400

    No actual need for Heading repository right now

--- src/main/java/edu/northeastern/cs4550/repositories/widget/HeadingRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.northeastern.cs4550.repositories.widget;
-
-import org.springframework.transaction.annotation.Transactional;
-
-import edu.northeastern.cs4550.models.widget.Heading;
-
-@Transactional
-public interface HeadingRepository extends BaseWidgetRepository<Heading> {
-}
