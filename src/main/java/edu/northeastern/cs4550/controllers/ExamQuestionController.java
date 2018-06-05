a269ceb023b0a2dd10bfe3ff1f6d5ae6f575f3cc
Joseph Donovan <joeydonovan4@gmail.com>
2018-06-05 17:38:28 -0400

    Exam question endpoints

---
+++ src/main/java/edu/northeastern/cs4550/controllers/ExamQuestionController.java
@@ -0,0 +1,34 @@
+package edu.northeastern.cs4550.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import edu.northeastern.cs4550.models.widget.exam.ExamQuestion;
+import edu.northeastern.cs4550.services.widget.exam.IExamQuestionService;
+
+@RestController
+@RequestMapping("/api/questions")
+public class ExamQuestionController {
+
+    @Autowired
+    private IExamQuestionService examQuestionService;
+
+    @GetMapping
+    public ResponseEntity<List<ExamQuestion>> findAllExamQuestions() {
+        List<ExamQuestion> examQuestions = examQuestionService.findAllExamQuestions();
+        return ResponseEntity.ok(examQuestions);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ExamQuestion> deleteExamQuestion(@PathVariable(value = "id") int questionId) {
+        ExamQuestion deletedQuestion = examQuestionService.deleteExamQuestion(questionId);
+        return ResponseEntity.ok(deletedQuestion);
+    }
+}
--- src/main/java/edu/northeastern/cs4550/controllers/TopicController.java
+++ src/main/java/edu/northeastern/cs4550/controllers/TopicController.java
@@ -2,6 +2,7 @@ package edu.northeastern.cs4550.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,8 +16,10 @@ import javax.validation.Valid;
 
 import edu.northeastern.cs4550.models.widget.Assignment;
 import edu.northeastern.cs4550.models.widget.Widget;
+import edu.northeastern.cs4550.models.widget.exam.ExamQuestion;
 import edu.northeastern.cs4550.services.widget.IAssignmentService;
 import edu.northeastern.cs4550.services.widget.IWidgetService;
+import edu.northeastern.cs4550.services.widget.exam.IExamQuestionService;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,6 +32,9 @@ public class TopicController {
 
     @Autowired
     private IAssignmentService assignmentService;
+
+    @Autowired
+    private IExamQuestionService examQuestionService;
 
     @PostMapping("/{id}/widgets")
     public ResponseEntity<List<Widget>> saveAllWidgets(@PathVariable(value = "id") int topicId,
@@ -43,6 +49,23 @@ public class TopicController {
                                                        @Valid @RequestBody Assignment a) {
         Assignment updatedAssignment = assignmentService.updateAssignment(topicId, assignmentId, a);
         return ResponseEntity.ok(updatedAssignment);
+    }
+
+    @PostMapping("/{tid}/exams/{eid}/questions")
+    public ResponseEntity<ExamQuestion> createExamQuestion(@PathVariable(value = "tid") int topicId,
+                                                           @PathVariable(value = "eid") int examId,
+                                                           @Valid @RequestBody ExamQuestion question) {
+        ExamQuestion examQuestion = examQuestionService.createExamQuestion(topicId, examId, question);
+        return ResponseEntity.ok(examQuestion);
+    }
+
+    @GetMapping("/{tid}/exams/{eid}/questions")
+    public ResponseEntity<List<ExamQuestion>> findAllExamQuestionsForTopicExam(
+            @PathVariable(value = "tid") int topicId, @PathVariable(value = "eid") int examId) {
+        List<ExamQuestion> examQuestions = examQuestionService.findAllExamQuestionsForTopicExam(
+                topicId, examId
+        );
+        return ResponseEntity.ok(examQuestions);
     }
 }
 
